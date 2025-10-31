cf9e540c7a16ddf1ffaafd4528db2491e8d94b26
Onkar <deshmukhonkar67@gmail.com>
2025-10-31 23:34:31 +0530

    void changed

--- frontend/src/pages/Void.jsx
+++ frontend/src/pages/Void.jsx
@@ -10,13 +10,13 @@ export default function Void() {
 
   return (
     <div className="h-screen w-screen bg-black flex items-center justify-center relative overflow-hidden">
-      <p
+      {/* <p
         className={`absolute bottom-4 right-6 text-[10px] text-gray-700 opacity-0 transition-opacity duration-[3000ms] ${
           visible ? "opacity-40" : "opacity-0"
         }`}
       >
         took you 330 million years to get out of here
-       </p>
+       </p> */}
     </div>
   );
 }
